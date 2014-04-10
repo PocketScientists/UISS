06cf7715245660dcadd92bf467200dffb9fffdfa
Philip Messlehner <mess.philip@gmail.com>
2014-04-10 11:58:25 +0200

    check if values are different before setting

--- Project/UISS/UILabel+UISS.m
+++ Project/UISS/UILabel+UISS.m
@@ -11,24 +11,26 @@ NSString *const UISSTextAttributeHighlightedTextColor = @"UISSTextAttributeHighl
 - (void)setTextAttributes:(NSDictionary *)textAttributes;
 {
     UIFont *font = [textAttributes objectForKey:UITextAttributeFont];
-    if (font) {
+    if (font != nil && self.font != font) {
         self.font = font;
     }
     UIColor *textColor = [textAttributes objectForKey:UITextAttributeTextColor];
-    if (textColor) {
+    if (textColor != nil && self.textColor != textColor) {
         self.textColor = textColor;
     }
     UIColor *textShadowColor = [textAttributes objectForKey:UITextAttributeTextShadowColor];
-    if (textShadowColor) {
+    if (textShadowColor != nil && self.shadowColor != textShadowColor) {
         self.shadowColor = textShadowColor;
     }
     NSValue *shadowOffsetValue = [textAttributes objectForKey:UITextAttributeTextShadowOffset];
     if (shadowOffsetValue) {
         UIOffset shadowOffset = [shadowOffsetValue UIOffsetValue];
-        self.shadowOffset = CGSizeMake(shadowOffset.horizontal, shadowOffset.vertical);
+        if (!UIOffsetEqualToOffset(shadowOffset, UIOffsetMake(self.shadowOffset.height, self.shadowOffset.width))) {
+            self.shadowOffset = CGSizeMake(shadowOffset.horizontal, shadowOffset.vertical);
+        }
     }
     UIColor *highlightedTextColor = [textAttributes objectForKey:UISSTextAttributeHighlightedTextColor];
-    if (highlightedTextColor) {
+    if (highlightedTextColor != nil && highlightedTextColor != self.highlightedTextColor) {
         self.highlightedTextColor = highlightedTextColor;
     }
 }
